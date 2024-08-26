32725867403df760ab304633f17b07e24418437e
walid2112 <walidahmed5w4@gmail.com>
2024-08-26 16:56:24 +0300

    second commit

--- main.c
+++ main.c
@@ -1,11 +1,12 @@
 
 
-
+#include <stdio.h>
 
 int main (void)
 {
 	
-	
+	int x=5;
+	int v=6;
 	
 	
 	return 0;
---
+++ programe1.c
@@ -0,0 +1,11 @@
+
+#include<stdio.h>
+int main (void)
+{
+	
+	
+	printf("Walid Ahmed");
+	
+	
+	return 0;
+}
